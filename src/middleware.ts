52fd24e15e7a45101aaa286a2da06baa8b1bae68
Derek J. Augustine <augshow@gmail.com>
2024-01-02 13:19:37 -0600

    add some stuff to the response 🔧

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,7 +1,21 @@
 import { NextRequest, NextResponse } from "next/server";
 
 export async function middleware(request: NextRequest) {
-  const response = NextResponse.next();
+  const requestHeaders = new Headers(request.headers);
+
+  // currently Next.js doesn't have a way to get the current path in
+  // a server component, this throws it into the headers for each
+  // request so we can grab it 😮‍💨
+  requestHeaders.set(
+    "x-current",
+    `${request.nextUrl.pathname}${request.nextUrl.search}`
+  );
+
+  const response = NextResponse.next({
+    request: {
+      headers: requestHeaders,
+    },
+  });
 
   return response;
 }
